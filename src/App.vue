f549d86199b0e22f5bcf17e4258c8d437ca8204a
Eugen Bezhenar <eugenbezhenar@MacBook-Air-Eugen.local>
2020-09-25 21:02:03 +0300

    Update Controls

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+<div class="container">
+<app-header :title="name" ></app-header>
+<hr>
+<div class="row">
+    <app-servers></app-servers>
+    <app-server-details ></app-server-details>
+</div>
+<hr>
+<app-footer></app-footer>
+</div>
+</template>
+
+<script>
+import Header from '@/components/Shared/Header'
+import Footer from '@/components/Shared/Footer'
+import Servers from '@/components/Server/Servers'
+import ServerDetails from '@/components/Server/ServerDetails'
+
+export default {
+components: {
+    'app-header': Header,
+    'app-servers': Servers,
+    'app-server-details': ServerDetails,
+    'app-footer': Footer
+    },
+    data: () => {
+        return {
+            name: false
+        }
+    }
+}
+
+
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Shared/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="row">
+        <div class="col-xs-12">
+            <header v-if="title">
+                <h1>Vue App {{title}}</h1>
+            </header>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'title'
+    ]
+}
+</script>
+
+<style>
+
+</style>
